2524a509401a8f0d70b1c8508f73d798bf6e349f
devlee77 <tjdcks0928@naver.com>
2021-12-08 01:29:13 +0900

    Add wpf_practice practice textbox, textblock, binding, button

---
+++ WpfApp_pratice/WpfApp_pratice/Cust.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_pratice
+{
+    public class Cust : INotifyPropertyChanged
+    {
+        private string name;
+        private int age;
+
+        public Cust() { }
+        public Cust(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; Notify("Name"); }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; Notify("Age"); }
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void Notify(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+    }
+}
---
+++ WpfApp_pratice/WpfApp_pratice/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_pratice
+{
+    
+
+    public partial class MainWindow : Window
+    {
+
+        Cust c1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            c1 = GetData();
+            ViewData(c1);
+
+            c1.PropertyChanged += C1_PropertyChanged;
+        }
+
+        private void C1_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Debug.WriteLine("39:" + e.PropertyName);
+            switch (e.PropertyName)
+            {
+                case "Name":
+                    txt_name.Text = c1.Name; break;
+                case "Age":
+                    txt_age.Text = c1.Age.ToString(); break;
+            }
+        }
+
+        private Cust GetData() 
+        {
+            Cust c = new Cust("홍길동", 27);
+            return c;
+        }
+
+        private void ViewData(Cust c1)
+        {
+            txt_name.Text = c1.Name;
+            txt_age.Text = c1.Age.ToString();
+        }
+
+        private void btn_year_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            int year = dt.Year - Convert.ToInt32(txt_age.Text) + 1;
+
+            MessageBox.Show("당신의 출생년도는 : " + year.ToString() + " 입니다.");
+        }
+
+        private void btn_addage_Click(object sender, RoutedEventArgs e)
+        {
+            c1.Age++;
+            //txt_age.Text = c1.Age.ToString();
+        }
+    }
+}
