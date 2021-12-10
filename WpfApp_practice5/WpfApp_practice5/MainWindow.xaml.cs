4686581643e036a5b35956b5be5b5f4132beba17
devlee77 <tjdcks0928@naver.com>
2021-12-10 17:46:34 +0900

    Add childwindow send data

---
+++ WpfApp_practice5/WpfApp_practice5/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_practice5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Window1 win1 = null;
+        private void Button_Click(object sender, RoutedEventArgs e)
+
+        {
+            if (win1 == null)
+            {
+                win1 = new Window1();
+                win1.OnchildTextnputEvent += new Window1.OnchildTextnputHandler(win1_OnchildTextnputEvent);
+                win1.Show();
+            }
+
+        }
+
+        void win1_OnchildTextnputEvent(string parameters)
+        {
+            textBlock.Text = parameters;
+            if (win1 != null)
+            {
+                win1.Close();
+                win1.OnchildTextnputEvent -= new Window1.OnchildTextnputHandler(win1_OnchildTextnputEvent);
+                win1 = null;
+            }
+        }
+    }
+}
---
+++ WpfApp_practice5/WpfApp_practice5/Window1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp_practice5
+{
+    /// <summary>
+    /// Window1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void OnchildTextnputHandler(string parameters);
+        public event OnchildTextnputHandler OnchildTextnputEvent;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (OnchildTextnputEvent != null)
+                OnchildTextnputEvent(textBox1.Text);
+        }
+    }
+}
